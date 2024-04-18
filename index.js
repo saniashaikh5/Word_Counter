a1ee72652f6d2c5f7ad68fd702174aa8dc5e0c87
saniashaikh5 <hurmen41@gmail.com>
2024-04-18 11:37:56 -0700

    word counter

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome To Word Counter\n");
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Sentence To Counte Your Words: "
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
